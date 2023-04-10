a52541fd95e424724ba0a306961960ef93948d29
alightbody <86264700+alightbody@users.noreply.github.com>
2023-04-10 16:38:35 -0400

    Add files via upload

--- seasonal_flow.py
+++ seasonal_flow.py
@@ -19,76 +19,76 @@ freq_vec : list of floats
     Frequency values to calculate and display 
 """
 
-#%% Import libraries -- Kush
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import pandas as pd
+#%% Import libraries 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
  
-#%% Input parameters -- Michael
-# infile_name = "lamprey_daily.txt" 
-# outfile_name = "lamprey_freqs.csv" 
-# freq_vec = [0.1, 0.25, 0.5, 0.75, 0.9]
-# title = 'Lamprey River, NH'
+#%% Input parameters
+infile_name = "lamprey_daily.txt" 
+outfile_name = "lamprey_freqs.csv" 
+freq_vec = [0.1, 0.25, 0.5, 0.75, 0.9]
+title = 'Lamprey River, NH'
 
-#%% Load USGS daily discharge data using read_csv -- Gavin
-# data = pd.read_csv(infile_name,delimiter="\t",comment='#',
+#%% Load USGS daily discharge data using read_csv 
+data = pd.read_csv(infile_name,delimiter="\t",comment='#',
                       na_values=['Bkw','Fld', 'Ice', 'Mnt', 'Rat', 'Zfl'])
 
 # Slice the resulting data frame to remove USGS format codes in first row
-# data = data.iloc[1:]     
+data = data.iloc[1:]     
           
-#%% Convert datetime column to dates, then set as index -- Andrew
-# data['datetime'] = pd.to_datetime(data['datetime'], format='%Y-%m-%d %H:%M')
-# data.set_index(['datetime'], inplace = True)
+#%% Convert datetime column to dates, then set as index 
+data['datetime'] = pd.to_datetime(data['datetime'], format='%Y-%m-%d %H:%M')
+data.set_index(['datetime'], inplace = True)
 
-#%% Wrangle discharge column -- Brian    
+#%% Wrangle discharge column 
 # Identify which column contains discharge data using the USGS code for 
 # discharge data, which is 00060. 
-# data = data.filter(like='00060',axis=1)  
+data = data.filter(like='00060',axis=1)  
 
 # Two columns contain this code: the discharge data, and a second with text 
 # flags about the discharge data. Keep the first column only (as a series)
-# data = data.iloc[:, 0]  
+data = data.iloc[:, 0]  
     
 # Convert discharge data to numeric data 
-# data = pd.to_numeric(data) 
+data = pd.to_numeric(data) 
 
-#%% Find first 10/1 and last 9/30 in record -- Jeremy
-# startdate = data.loc[(data.index.month == 10) & (data.index.day == 1)].index[0]
-# enddate = data.loc[(data.index.month == 9) & (data.index.day == 30)].index[-1]
+#%% Find first 10/1 and last 9/30 in record
+startdate = data.loc[(data.index.month == 10) & (data.index.day == 1)].index[0]
+enddate = data.loc[(data.index.month == 9) & (data.index.day == 30)].index[-1]
 
-#%% Trim data to whole water years -- Kaylee
-# data = data[startdate:enddate]   # contains discharge data in cfs 
+#%% Trim data to whole water years 
+data = data[startdate:enddate]   # contains discharge data in cfs 
 
-#%% Initialize empty data frame for results -- Kevin
-# maxdoy = 366  # Maximum number of days in a year
-# dfcum = pd.DataFrame(np.full((maxdoy,len(freq_vec)),np.nan),
-#                      columns=freq_vec,index=np.arange(1,maxdoy+1))
+#%% Initialize empty data frame for results 
+maxdoy = 366  # Maximum number of days in a year
+dfcum = pd.DataFrame(np.full((maxdoy,len(freq_vec)),np.nan),
+                     columns=freq_vec,index=np.arange(1,maxdoy+1))
 
-#%% Average seasonal record over all years -- Nick
-# for freq in freq_vec:
-#     dfcum[freq] = data.groupby(data.index.dayofyear).quantile(freq)
+#%% Average seasonal record over all years 
+for freq in freq_vec:
+    dfcum[freq] = data.groupby(data.index.dayofyear).quantile(freq)
 
-#%% Create bonus rows containing NaN's in between each year -- Madison
-# newdates = pd.date_range(startdate,enddate,freq = 'Y')+pd.Timedelta(1, unit='hour')
-# newvalues = pd.Series(np.full(len(newdates),np.nan),index=newdates)
+#%% Create bonus rows containing NaN's in between each year 
+newdates = pd.date_range(startdate,enddate,freq = 'Y')+pd.Timedelta(1, unit='hour')
+newvalues = pd.Series(np.full(len(newdates),np.nan),index=newdates)
 
-#%% Add bonus rows to data series (to avoid plotting overlap) -- Nora
-# data = pd.concat([data,newvalues])
-# data.sort_index(inplace = True)
+#%% Add bonus rows to data series (to avoid plotting overlap) 
+data = pd.concat([data,newvalues])
+data.sort_index(inplace = True)
 
-#%% Plot time series of seasonal discharge -- Jon-Erik
-# fig, ax = plt.subplots(nrows = 1, ncols = 1)
-# ax.plot(data.index.dayofyear,data,'-',
-#        color = 'gray',label = 'Historical')
-# for freq in freq_vec:
-#     ax.plot(dfcum[freq],'-', label = f'{freq*100:.0f}th')
-# ax.set_ylabel('Discharge (cfs)')
-# ax.set_xlabel('Day of year')
-# ax.set_xlim(left = 0, right = 366)
-# ax.set_yscale('log')
-# ax.legend()
-# ax.set_title(title)
+#%% Plot time series of seasonal discharge
+fig, ax = plt.subplots(nrows = 1, ncols = 1)
+ax.plot(data.index.dayofyear,data,'-',
+        color = 'gray',label = 'Historical')
+for freq in freq_vesc:
+    ax.plot(dfcum[freq],'-', label = f'{freq*100:.0f}th')
+ax.set_ylabel('Discharge (cfs)')
+ax.set_xlabel('Day of year')
+ax.set_xlim(left = 0, right = 366)
+ax.set_yscale('log')
+ax.legend()
+ax.set_title(title)
 
-#%% Save results to file -- Noah
-# dfcum.to_csv(outfile_name,sep=',')
+#%% Save results to file 
+dfcum.to_csv(outfile_name,sep=',')
